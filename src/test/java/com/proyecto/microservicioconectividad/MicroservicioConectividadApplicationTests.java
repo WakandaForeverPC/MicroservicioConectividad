5d41ae72f44a6fa06b79045a77f4018611d92d9f
Samuu10 <svallmun@myuax.com>
2024-12-10 18:24:40 +0100

    Initial commit

---
+++ src/test/java/com/proyecto/microservicioconectividad/MicroservicioConectividadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioconectividad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioConectividadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
